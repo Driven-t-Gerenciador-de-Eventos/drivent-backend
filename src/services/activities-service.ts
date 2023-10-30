ca62fbc410278f2f03a3b5d3669a40bb6c42c511
rafaelanobre <raafanobre@gmail.com>
2023-10-30 10:26:31 -0300

    Get activities with is isSubscribed

--- src/services/activities-service.ts
+++ src/services/activities-service.ts
@@ -25,7 +25,7 @@ function formatarData(data: string) {
 async function getActivities(userId: number) {
   await validateUserBooking(userId);
 
-  const activities = await activitiesRepository.findActivities();
+  const activities = await activitiesRepository.findActivities(userId);
   if (activities.length === 0) throw notFoundError();
 
   const activitiesWithFormattedDate = activities.map(activity => ({
@@ -51,28 +51,34 @@ async function getActivityById(activityId: number){
 
 async function reserveActivity(userId: number, activityId: number) {
   await validateUserBooking(userId);
-
+  console.log('passou na validacao do user')
   if (!activityId || isNaN(activityId)) throw invalidDataError('activityId');
+  console.log('id veio certo')
 
   const activity = await getActivityById(activityId);
   if (activity.Reservation.length >= activity.capacity) {
     throw conflictError('Atividade sem vagas');
   }
-
+  console.log('passou no numero de vagas vagas')
 
   const userActivities = await getActivitiesByUser(userId, activity.date);
+  console.log('pegando as atividade do user')
 
   if(userActivities){
+    console.log('entrou no if')
     const conflictActivity = userActivities.find(userActivity =>
       userActivity.Activity.startsAt <= activity.endsAt && userActivity.Activity.endsAt >= activity.startsAt
     );
+    console.log('passou no find')
+    console.log(conflictActivity)
     if (conflictActivity) {
       throw conflictError('Atividades no mesmo horário');
     }
+    console.log('passou na validacao de horarios')
   }
 
   const reservation = await activitiesRepository.reserveActivity(userId, activityId);
-
+  console.log('fazendo reserva')
   return reservation.id;
 }
 
